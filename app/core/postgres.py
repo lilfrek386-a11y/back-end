7e9218e7915d086108110d66d25d3ca8b7c2a07d
Istoshin Pavlo <pavloistoshin@gmail.com>
2026-05-27 02:32:55 +0300

    Create connection to postgres

---
+++ app/core/postgres.py
@@ -0,0 +1,17 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from app.core.config import settings
+
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    # echo=True
+)
+
+session_factory = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+async def get_session() -> AsyncSession:
+    async with session_factory() as session:
+        yield session
