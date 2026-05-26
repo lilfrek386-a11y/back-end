3ee147943e33d343113e80889d9d05c023175b46
Istoshin Pavlo <pavloistoshin@gmail.com>
2026-05-26 16:54:06 +0300

    Implement CORSmiddleware to handle cross-origin requests properly

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI, status
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/", status_code=status.HTTP_200_OK)
 async def health_check():
