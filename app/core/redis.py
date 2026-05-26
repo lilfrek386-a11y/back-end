0f4a28a4cbdc99660abab7cb51995f7a58aac270
Istoshin Pavlo <pavloistoshin@gmail.com>
2026-05-27 02:32:44 +0300

    Create connection to redis

---
+++ app/core/redis.py
@@ -0,0 +1,11 @@
+import redis.asyncio as redis
+from app.core.config import settings
+
+redis_client = redis.Redis(
+    host=settings.REDIS_HOST,
+    port=settings.REDIS_PORT,
+    decode_responses=True
+)
+
+async def get_redis():
+    yield redis_client
