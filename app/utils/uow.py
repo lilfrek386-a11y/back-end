0345663bfb82b064e518b2aba7d0deb425988d31
Istoshin Pavlo <pavloistoshin@gmail.com>
2026-05-28 14:20:29 +0300

    Add UnitOfWork

---
+++ app/utils/uow.py
@@ -0,0 +1,22 @@
+from app.core.postgres import session_factory
+from app.repositories.user import UserRepository
+
+
+class UnitOfWork:
+    def __init__(self, session_factory=session_factory):
+        self._session_factory = session_factory
+
+    async def __aenter__(self):
+        self.session = self._session_factory()
+        self.users = UserRepository(self.session)
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, tb):
+        try:
+            if exc_type is None:
+                await self.session.commit()
+            else:
+                await self.session.rollback()
+        finally:
+            await self.session.close()
+            self.session = None
