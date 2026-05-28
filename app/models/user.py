54ccb9e01badad6a5679b6cef6358affb1d79dd0
Istoshin Pavlo <pavloistoshin@gmail.com>
2026-05-28 17:32:51 +0300

    Add mixin CreatedAt to User model

--- app/models/user.py
+++ app/models/user.py
@@ -2,13 +2,17 @@ from sqlalchemy import String, Integer
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.models.base import Base
+from app.models.mixins import CreatedAtMixin
 
-class User(Base):
-    __tablename__ = 'users'
+
+class User(Base, CreatedAtMixin):
+    __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(50))
-    email: Mapped[str] = mapped_column(String(), unique=True, index=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(), unique=True, index=True, nullable=False
+    )
     hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
     age: Mapped[int | None] = mapped_column(Integer, nullable=True)
 
