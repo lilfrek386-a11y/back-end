2fdf012bbf6a26e63f9ed3da3a0f10b178663aa8
Istoshin Pavlo <pavloistoshin@gmail.com>
2026-05-28 13:46:51 +0300

    Add models package and user model

---
+++ app/models/user.py
@@ -0,0 +1,16 @@
+from sqlalchemy import String, Integer
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.core.base import Base
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50))
+    email: Mapped[str] = mapped_column(String(), unique=True, index=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
+    age: Mapped[int | None] = mapped_column(Integer, nullable=True)
+
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, name={self.name!r}, email={self.email!r})"
