6a8df96d386e64f949753f6b4f70afc58c5a3d65
Istoshin Pavlo <pavloistoshin@gmail.com>
2026-06-03 19:17:32 +0300

    Update mixins

--- app/models/mixins.py
+++ app/models/mixins.py
@@ -1,16 +1,16 @@
-from sqlalchemy import func
 from datetime import datetime, timezone
-
 from sqlalchemy.orm import Mapped, mapped_column
 
 
+def get_utcnow() -> datetime:
+    return datetime.now(timezone.utc).replace(tzinfo=None)
+
+
 class CreatedAtMixin:
-    created_at: Mapped[datetime] = mapped_column(
-        server_default=func.now(), default=lambda: datetime.now(timezone.utc)
-    )
+    created_at: Mapped[datetime] = mapped_column(default=get_utcnow)
 
 
 class TimestampMixin(CreatedAtMixin):
     updated_at: Mapped[datetime] = mapped_column(
-        server_default=func.now(), onupdate=func.now()
+        default=get_utcnow, onupdate=get_utcnow, nullable=True
     )
