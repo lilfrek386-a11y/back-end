9e8abfe912fd8be724d25335429f447aa8a76258
Istoshin Pavlo <pavloistoshin@gmail.com>
2026-06-06 23:18:49 +0300

    Update mixin

--- app/models/mixins.py
+++ app/models/mixins.py
@@ -1,5 +1,7 @@
-from datetime import datetime, timezone
+import uuid
+from sqlalchemy import UUID
 from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime, timezone
 
 
 def get_utcnow() -> datetime:
@@ -8,3 +10,9 @@ def get_utcnow() -> datetime:
 
 class TimestampMixin:
     created_at: Mapped[datetime] = mapped_column(default=get_utcnow)
+
+
+class UUIDMixin:
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
+    )
