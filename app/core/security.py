a7a7dab4e5f0b95bb8b41adb25bf499dae1c3089
Istoshin Pavlo <pavloistoshin@gmail.com>
2026-06-08 01:32:34 +0300

    Implement JWT creation and Auth0 token verification

--- app/core/security.py
+++ app/core/security.py
@@ -1,17 +1,66 @@
-import bcrypt
+import jwt
+from jwt import PyJWKClient
+from datetime import timedelta, datetime, timezone
+from pwdlib import PasswordHash
+from app.core.config import settings
+from fastapi import HTTPException, status
+
+password_hash = PasswordHash.recommended()
 
 
 def get_password_hash(password: str) -> str:
-    password_bytes = password.encode("utf-8")
-
-    salt = bcrypt.gensalt()
-    hashed_bytes = bcrypt.hashpw(password_bytes, salt)
-
-    return hashed_bytes.decode("utf-8")
+    return password_hash.hash(password)
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    password_bytes = plain_password.encode("utf-8")
-    hashed_password_bytes = hashed_password.encode("utf-8")
+    return password_hash.verify(plain_password, hashed_password)
 
-    return bcrypt.checkpw(password_bytes, hashed_password_bytes)
+
+def create_access_token(user_data: dict, expires_delta: timedelta | None = None) -> str:
+    to_encode = {
+        "sub": user_data.get("sub") or str(user_data.get("id")),
+        "email": user_data["email"],
+    }
+
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(
+            minutes=settings.jwt.ACCESS_TOKEN_EXPIRE_MINUTES
+        )
+
+    to_encode.update({"exp": int(expire.timestamp())})
+
+    encoded_jwt = jwt.encode(
+        to_encode, settings.jwt.SECRET_KEY, algorithm=settings.jwt.ALGORITHM
+    )
+
+    return encoded_jwt
+
+
+jwks_url = f"https://{settings.auth0.DOMAIN}/.well-known/jwks.json"
+jwks_client = PyJWKClient(jwks_url)
+
+
+def verify_auth0_token(token: str) -> dict:
+    try:
+        signing_key = jwks_client.get_signing_key_from_jwt(token)
+
+        payload = jwt.decode(
+            token,
+            signing_key.key,
+            algorithms=["RS256"],
+            audience=settings.auth0.AUDIENCE,
+            issuer=f"https://{settings.auth0.DOMAIN}/",
+        )
+        return payload
+
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Auth0 token expired"
+        )
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"Invalid Auth0 token: {str(e)}",
+        )
